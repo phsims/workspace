2a567a306226886e06068fd8dece9bf4e4c95bf6
pam-ceracare <155556709+pam-ceracare@users.noreply.github.com>
2024-05-07 14:02:23 +0100

    updates

--- libs/components/src/index.ts
+++ libs/components/src/index.ts
@@ -1,5 +1,5 @@
 // Use this file to export React client components (e.g. those with 'use client' directive) or other non-server utilities
-export * from  './lib/Organisms/Features/Features';
-export * from  './lib/Organisms/Header/Header';
-export * from  './lib/Organisms/RecipeCard/RecipeCard';
+export * from  './lib/organisms/Features/Features';
+export * from  './lib/organisms/Header/Header';
+export * from  './lib/organisms/RecipeCard/RecipeCard';
 export * from './lib/themes/ThemeProvider';
