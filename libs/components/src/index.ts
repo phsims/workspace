8f45c9fd6c3453a4446962b86cb20ad5016da799
Pam Halligan-Sims <pamhsims@yahoo.com>
2024-05-15 10:41:35 +0100

    removed features, added feature card, updated button link

--- libs/components/src/index.ts
+++ libs/components/src/index.ts
@@ -1,7 +1,6 @@
 export * from './lib/organisms/FeatureCard/FeatureCard';
 export * from './lib/atoms/ButtonLink/ButtonLink';
 export * from './lib/organisms/Banner/Banner';
-export * from './lib/organisms/Features/Features';
 export * from './lib/organisms/Header/Header';
 export * from './lib/organisms/RecipeCard/RecipeCard';
 export * from './lib/themes/ThemeProvider';
