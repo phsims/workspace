13b49f82aaf3f8c75cf4d18fd4f95eef94500bb0
Pam Halligan-Sims <pamhsims@yahoo.com>
2024-05-31 15:45:37 +0100

    default page states

--- apps/plan-my-plate/app/layout.tsx
+++ apps/plan-my-plate/app/layout.tsx
@@ -1,8 +1,12 @@
+import { Suspense } from 'react';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
+
 import CssBaseline from '@mui/material/CssBaseline';
 import { CustomThemeProvider, Header } from '@workspace/components';
 import LocalDiningRoundedIcon from '@mui/icons-material/LocalDiningRounded';
 import DescriptionRoundedIcon from '@mui/icons-material/DescriptionRounded';
+import Loading from './loading';
+
 
 export const metadata = {
   title: 'Welcome to plan-my-plate',
@@ -35,7 +39,9 @@ export default function RootLayout({
                 />
               }
             />
-            {children}
+            <Suspense fallback={<Loading />}>
+              {children}
+            </Suspense>
           </CustomThemeProvider>
         </AppRouterCacheProvider>
       </body>
